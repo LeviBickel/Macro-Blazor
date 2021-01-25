09e568de2cf63f03eb39f3824fdd81c25c2197f4
Levi <bickelsolutions@gmail.com>
2021-01-25 10:21:49 -0700

    Added a card to insert items into the Software table.

--- MacroUI/Models/SoftwareDisplayModel.cs
+++ MacroUI/Models/SoftwareDisplayModel.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace DataAccess.Models
+namespace MacroUI.Models
 {
-    public class SoftwareModel
+    public class SoftwareDisplayModel
     {
         public int ID { get; set; }
+        [Required]
         public string Manufacturer { get; set; }
-        
+        [Display(Name = "Software Title")]
+        [Required]
         public string SoftwareTitle { get; set; }
+        [Display(Name = "Installed on")]
         public string AssignedTo { get; set; }
+        [Display(Name = "Purchase Order")]
         public string PurchaseOrder { get; set; }
+        [Display(Name = "License Type")]
         public string LicenseType { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTime PurchaseDate { get; set; }
         public bool Supported { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTime SupportExp { get; set; }
+        [Display(Name = "Total Keys")]
+        [Required]
         public int TotalKeys { get; set; }
+        [Display(Name = "Used Keys")]
+        [Required]
         public int UsedKeys { get; set; }
+        [Display(Name = "License Key")]
         public string LicenseKey { get; set; }
+        [Display(Name = "Date Added")]
+        [DataType(DataType.DateTime)]
         public DateTime Added { get; set; }
     }
 }
