d9a0570c38bd14093b4c5b8c5cebaa45b83c91c5
Levi <bickelsolutions@gmail.com>
2021-01-25 10:15:43 -0700

    removed comments on Software Model

--- DataAccess/Models/SoftwareModel.cs
+++ DataAccess/Models/SoftwareModel.cs
@@ -9,27 +9,16 @@ namespace DataAccess.Models
     {
         public int ID { get; set; }
         public string Manufacturer { get; set; }
-        //[Display(Name = "Software Title")]
         public string SoftwareTitle { get; set; }
-        //[Display(Name = "Installed on")]
         public string AssignedTo { get; set; }
-        //[Display(Name = "Purchase Order")]
         public string PurchaseOrder { get; set; }
-        //[Display(Name = "License Type")]
         public string LicenseType { get; set; }
-        //[DataType(DataType.DateTime)]
         public DateTime PurchaseDate { get; set; }
         public bool Supported { get; set; }
-        //[DataType(DataType.DateTime)]
         public DateTime SupportExp { get; set; }
-        //[Display(Name = "Total Keys")]
         public int TotalKeys { get; set; }
-        //[Display(Name = "Used Keys")]
         public int UsedKeys { get; set; }
-        //[Display(Name = "License Key")]
         public string LicenseKey { get; set; }
-        //[Display(Name = "Date Added")]
-        //[DataType(DataType.DateTime)]
         public DateTime Added { get; set; }
     }
 }
